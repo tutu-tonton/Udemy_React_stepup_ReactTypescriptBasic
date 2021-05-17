b0a2c23e27b1f46924955eec881ed8f0fccf87b9
tutu-tonton <50165379+tutu-tonton@users.noreply.github.com>
2021-05-17 19:24:52 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+import { Practice1 } from "./practices/Practice1";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Practice1 />
+    </div>
+  );
+}
